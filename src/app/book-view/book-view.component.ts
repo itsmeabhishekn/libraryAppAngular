d6604a0e5f5d6d502c95dee5dca77b120d6134df
itsmeabhishekn <abhishekn4454@gmail.com>
2022-11-25 11:23:36 +0530

    Added data binding

--- src/app/book-view/book-view.component.ts
+++ src/app/book-view/book-view.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./book-view.component.css']
 })
 export class BookViewComponent {
+data:any = [
+  {"src":"https://images-platform.99static.com//9ogHx5JG1u-3oVmBDpICV4OB9PA=/60x52:1882x1874/fit-in/500x500/99designs-contests-attachments/123/123255/attachment_123255102",
+  "title":"book1",
+  "description":"This is a nice book",
+  "price":120
+},{"src":"https://images-platform.99static.com//9ogHx5JG1u-3oVmBDpICV4OB9PA=/60x52:1882x1874/fit-in/500x500/99designs-contests-attachments/123/123255/attachment_123255102",
+"title":"book1",
+"description":"This is a nice book",
+"price":120
+},{"src":"https://images-platform.99static.com//9ogHx5JG1u-3oVmBDpICV4OB9PA=/60x52:1882x1874/fit-in/500x500/99designs-contests-attachments/123/123255/attachment_123255102",
+"title":"book1",
+"description":"This is a nice book",
+"price":120
+},{"src":"https://images-platform.99static.com//9ogHx5JG1u-3oVmBDpICV4OB9PA=/60x52:1882x1874/fit-in/500x500/99designs-contests-attachments/123/123255/attachment_123255102",
+"title":"book1",
+"description":"This is a nice book",
+"price":120
+},{"src":"https://images-platform.99static.com//9ogHx5JG1u-3oVmBDpICV4OB9PA=/60x52:1882x1874/fit-in/500x500/99designs-contests-attachments/123/123255/attachment_123255102",
+"title":"book1",
+"description":"This is a nice book",
+"price":120
+},{"src":"https://images-platform.99static.com//9ogHx5JG1u-3oVmBDpICV4OB9PA=/60x52:1882x1874/fit-in/500x500/99designs-contests-attachments/123/123255/attachment_123255102",
+"title":"book1",
+"description":"This is a nice book",
+"price":120
+},{"src":"https://images-platform.99static.com//9ogHx5JG1u-3oVmBDpICV4OB9PA=/60x52:1882x1874/fit-in/500x500/99designs-contests-attachments/123/123255/attachment_123255102",
+"title":"book1",
+"description":"This is a nice book",
+"price":120
+},
 
+]
 }
